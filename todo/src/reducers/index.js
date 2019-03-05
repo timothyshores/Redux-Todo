2e1bbbf338bdbfa36d180cf8633d111b78bb6fe4
Timothy Shores <timothymshores@gmail.com>
2019-03-05 17:39:05 -0600

    Completed todo app

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,15 +1,8 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/App'
-import rootReducer from './reducers'
+import { combineReducers } from 'redux'
+import todos from './todos'
+import visibilityFilter from './visibilityFilter'
 
-const store = createStore(rootReducer)
-
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+export default combineReducers({
+  todos,
+  visibilityFilter
+})
